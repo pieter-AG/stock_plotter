c829a61db5f8b0afe33e906ed7c7af2333208714
Pieter Van Wyk <pietervw@advanceguidance.com>
2022-01-19 23:18:37 +0200

    optimised date selection - core mechanics now finalised

--- stockplotter.py
+++ stockplotter.py
@@ -7,60 +7,91 @@ from dash import html
 import pandas_datareader.data as web
 from datetime import date
 
-app = dash.Dash(__name__)
 # Fetches all tickers listed on nasdaq
 all_tickers = ['AAPL', 'AMZN', 'GOOG', 'TSLA']
 
 # Starting graph attributes
+start_date = '2022-01-01'
+end_date = '2022-01-18'
 start_ticker = all_tickers[0]
-start_df = web.DataReader(all_tickers[0], 'stooq', start='2022-01-01', end='2022-01-18')
+start_df = web.DataReader(all_tickers[0], 'stooq', start=start_date, end=end_date)
 start_trace = go.Scatter(x=start_df.index, y=start_df.Close, mode='lines+markers', name=start_ticker)
 
 # List of dictionaries containing trace dataframe and ticker
 stocks = [{
     'ticker': start_ticker,
+    'start': start_date,
+    'end': end_date,
     'df': start_df
 }]
 
+app = dash.Dash(__name__)
 app.layout = html.Div([
     html.H1("Select a company's ticker from the dropdown below"),
-    dcc.Dropdown(
-        id='ticker-dropdown',
-        options=[{'label': ticker, 'value': ticker} for ticker in all_tickers],
-        value=[start_ticker],
-        multi=True
-    ),
+    html.Div([
+        dcc.Dropdown(
+            id='ticker-dropdown',
+            options=[{'label': ticker, 'value': ticker} for ticker in all_tickers],
+            value=[start_ticker],
+            multi=True
+        ),
+    ], style={'width': '40%', 'padding': '0.5%'}),
+    html.Div([
+        dcc.DatePickerRange(
+        id='date-picker-range',
+        min_date_allowed=date(2000, 1, 1),
+        max_date_allowed=date.today(),
+        start_date=start_date,
+        end_date=end_date,
+        ),
+    ], style={'width': '20%', 'padding': '0.5%'}),
     html.Br(),
     html.Div(id='my-output'),
     dcc.Graph(
         id='stock-graph',
-
-        # Starting trace
         figure= go.Figure(data=start_trace),
     )
-])
+], style={'padding': '1%', 'margin': '0 auto'})
 
 @app.callback(
     Output(component_id='my-output', component_property='children'),
     Output('stock-graph', 'figure'),
-    Input(component_id='ticker-dropdown', component_property='value')
+    Input(component_id='ticker-dropdown', component_property='value'),
+    Input(component_id='date-picker-range', component_property='start_date'),
+    Input(component_id='date-picker-range', component_property='end_date')
 )
-def update_output_div(tickers):
+def update_output_div(tickers, pstart, pend):
+    # Update stocks' dictionaries if start/ end date has changed
+    for c, stock in enumerate(stocks[:]):
+        if not stock['start'] == pstart or not stock['end'] == pend:
+            stocks.pop(c)           # pop and inserting in for-loop is probably not 
+            stocks.insert(c, {         # best practice, but anyways...
+                'ticker': stock['ticker'],      
+                'start': pstart,
+                'end': pend,
+                'df': web.DataReader(stock['ticker'], 'stooq', start=pstart, end=pend)
+            })
+
     # Add new dictionary to stocks, if not allready in stocks
+    tickers_update = []         # List of stocks that require a date-range update
     for ticker in tickers:
         if ticker not in [stock['ticker'] for stock in stocks]:
             stocks.append({
                 'ticker': ticker,
-                'df': web.DataReader(ticker, 'stooq', start='2022-01-01', end='2022-01-18')
+                'start': pstart,
+                'end': pend,
+                'df': web.DataReader(ticker, 'stooq', start=pstart, end=pend)
             })
 
     # Create list of traces which will be plotted
     traces = []
     for stock in stocks:
         if stock['ticker'] in tickers:
-            traces.append(go.Scatter(x=stock['df'].index, y=stock['df'].Close, mode='lines+markers', name=stock['ticker']))
+            traces.append(go.Scatter(x=stock['df'].index, y=stock['df'].Close, 
+                                     mode='lines+markers', name=stock['ticker']))
 
-    fig = go.Figure(data=traces)
+    fig = go.Figure(data=traces, 
+                    layout=go.Layout(title=f'Stock price through interval {pstart} / {pend}'))
     return 'Selected Tickers: {}'.format(tickers), fig
 
 
--- tester1.py
+++ tester1.py
@@ -1,18 +1,45 @@
 dicts = [
-    { "name": "Tom", "age": 10 },
-    { "name": "Mark", "age": 5 },
-    { "name": "Pam", "age": 7 },
-    { "name": "Dick", "age": 12 }
+    { "name": "a", "age": 10 },
+    { "name": "b", "age": 10 },
+    { "name": "c", "age": 10 },
+    { "name": "d", "age": 12 },
+    { "name": "e", "age": 10 },
+    { "name": "f", "age": 5 },
+    { "name": "g", "age": 10 },
+    { "name": "h", "age": 12 },
 ]
 print(dicts)
+pop_list = []
+for c, d in enumerate(dicts[:]):
+    if d['age'] == 10:
+        dicts.pop(c)
+        dicts.insert(c, {'name': d['name'], 'age': 11})
+        print(c)
 
-new = [d for d in dicts if d.get('name') != 'Pam']
-print(new)
+print(dicts)
 
-print(new[0]['name'])
 
-keys = ['Dick', 'Piet', 'Mark', 'Dave']
+# pop_list = []
+# for c, d in enumerate(dicts, start=0):
+#     if d['age'] == 11:
+#         dicts.append({'name': d['name'], 'age': 0})
+#         print(c)
+#         pop_list.append(c)
+        
+# pop_list.reverse()
+# for c in pop_list:
+#     dicts.pop(c)
+    
+# print(dicts)
+# print(next(x for x in dicts if x["name"] == "Pam"))
 
-for key in keys:
-    if key not in [d['name'] for d in dicts]:
-        print(f'{key} is not in the list')
+# new = [d for d in dicts if d.get('name') != 'Pam']
+# print(new)
+
+# print(new[0]['name'])
+
+# keys = ['Dick', 'Piet', 'Mark', 'Dave']
+
+# for key in keys:
+#     if key not in [d['name'] for d in dicts]:
+#         print(f'{key} is not in the list')
